f6ad3b2ad41b72a62515135886b736883a704adc
vineethk123 <vineeth.kolluru@zemosolabs.com>
2019-01-26 20:25:13 +0530

    make items in menu draggable and add corresponding drag event listeners

--- js/index.js
+++ js/index.js
@@ -1,3 +1,36 @@
+const menuContainerDiv = document.getElementById("menu-div");
+const itemDivs = menuContainerDiv.querySelectorAll("div");
+const tableContainerDiv = document.getElementById("all-tables-div");
+const tableDivs = tableContainerDiv.querySelectorAll("div");
+
+// Add event listeners to table and item div elements
+for (const div of itemDivs) {
+  div.addEventListener("dragstart", dragStart);
+  div.addEventListener("dragend", dragEnd);
+}
+
+for (const div of tableDivs) {
+  div.addEventListener("dragover", dragOver);
+  div.addEventListener("drop", dragDrop);
+}
+
+function dragStart(dragEvent) {
+  console.log("In dragStart");
+}
+
+function dragEnd(dragEvent) {
+  console.log("In dragEnd");
+}
+
+function dragOver(event) {
+  // Necessary to trigger 'drop' event
+  event.preventDefault();
+}
+
+function dragDrop(event) {
+  console.log("in drag drop");
+}
+
 /**
  * Takes parent div id as parameter, fetches the search keyword &
  * filters the child divs(the contents) accordingly.
