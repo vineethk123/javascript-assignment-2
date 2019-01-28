3fa793d516d87ccf57f35c5926eefb564d204068
vineethk123 <vineeth.kolluru@zemosolabs.com>
2019-01-28 15:10:38 +0530

    use a variable to keep track of the orders for all tables and use render method to update a table's price and item-count.

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ const tableContainerDiv = document.getElementById("all-tables-div");
 const tableDivs = tableContainerDiv.querySelectorAll("div");
 let modalElement = document.getElementsByClassName("modal")[0];
 let currentTablediv; // div element for which the pop-up is being displayed
+let allTableOrders = {};
 
 // Add event listeners to table and item div elements
 for (const div of itemDivs) {
@@ -21,11 +22,16 @@ for (const div of tableDivs) {
  */
 function dragStart(dragEvent) {
   let draggedItemElement = dragEvent.srcElement;
+  let draggedItemName = draggedItemElement.querySelector("p.card-title")
+    .innerText;
   let draggedItemPrice = draggedItemElement.querySelector("p.card-content")
     .innerText;
   // dataTransfer property is used to hold the data that is being dragged.
   // setData() is used to store the data in the dataTranfer object during drag operation.
-  dragEvent.dataTransfer.setData("text/plain", draggedItemPrice);
+  dragEvent.dataTransfer.setData(
+    "text/plain",
+    draggedItemName + "," + draggedItemPrice
+  );
 }
 
 /**
@@ -39,19 +45,65 @@ function dragOver(dragEvent) {
 
 /**
  * Triggered when the dragged element is dropped on an element with 'drop' event listener attached.
- * Increments the item-count by 1 and adds the dragged item's price to total price of the target table.
+ * Populates the allTableOrders as per the dragged item and dropped table.
  * @param {Object} draggableEvent
  */
 function dragDrop(dragEvent) {
-  let priceSpanElement = this.querySelector("span[data-name='price']");
-  let itemCountSpanElement = this.querySelector("span[data-name='item-count']");
-  let draggedItemPrice = parseFloat(
-    dragEvent.dataTransfer.getData("text/plain")
+  let draggedItemContent = dragEvent.dataTransfer
+    .getData("text/plain")
+    .split(",");
+  let draggedItemName = draggedItemContent[0];
+  let draggedItemPrice = parseFloat(draggedItemContent[1]);
+  let droppedTableId = this.getAttribute("id");
+
+  // Populate a table's new order in allTableOrders
+  // Create an entry for a table along with the dragged item if no orders were placed for that table.
+  // If a new item is added to an existing table make an entry for it.
+  // If an existing item is ordered in an existing table increment the count of that item for that table by 1.
+  if (typeof allTableOrders[droppedTableId] === "undefined") {
+    allTableOrders[droppedTableId] = {};
+    allTableOrders[droppedTableId][draggedItemName] = {};
+    allTableOrders[droppedTableId][draggedItemName]["price"] = draggedItemPrice;
+    allTableOrders[droppedTableId][draggedItemName]["count"] = 1;
+  } else if (
+    typeof allTableOrders[droppedTableId][draggedItemName] === "undefined"
+  ) {
+    allTableOrders[droppedTableId][draggedItemName] = {};
+    allTableOrders[droppedTableId][draggedItemName]["price"] = draggedItemPrice;
+    allTableOrders[droppedTableId][draggedItemName]["count"] = 1;
+  } else {
+    allTableOrders[droppedTableId][draggedItemName]["count"] += 1;
+  }
+  renderTableContent(this);
+}
+
+/**
+ * Render/update a single table's content from 'allTableOrders' - price and count
+ * @param {object} targetTableElement
+ * @param {object} dragEvent
+ */
+function renderTableContent(targetTableElement) {
+  console.log("in rendertablecontent");
+  let tableBill = 0,
+    tableItemCount = 0;
+  let targetTableId = targetTableElement.getAttribute("id");
+  let priceSpanElement = targetTableElement.querySelector(
+    "span[data-name='price']"
   );
-  let newPrice = parseFloat(priceSpanElement.innerText) + draggedItemPrice;
-  priceSpanElement.innerText = newPrice;
-  let tableItemCount = parseInt(itemCountSpanElement.innerText);
-  itemCountSpanElement.innerText = tableItemCount + 1;
+  let itemCountSpanElement = targetTableElement.querySelector(
+    "span[data-name='item-count']"
+  );
+  console.log("Elements: ", priceSpanElement, itemCountSpanElement);
+  for (let item in allTableOrders[targetTableId]) {
+    console.log("item: ", allTableOrders[targetTableId][item]);
+    tableBill +=
+      allTableOrders[targetTableId][item].price *
+      allTableOrders[targetTableId][item].count;
+    tableItemCount += allTableOrders[targetTableId][item].count;
+  }
+  console.log("Bill: ", tableBill, "\nitemcount: ", tableItemCount);
+  priceSpanElement.innerText = tableBill;
+  itemCountSpanElement.innerText = tableItemCount;
 }
 
 /**
